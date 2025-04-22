f9b31cc5d14cbfa8df852b7cb02b402fc61a89b0
한지원 <140470794+hanjiwon1108@users.noreply.github.com>
2025-04-23 01:22:23 +0900

    stramlit.py icon 제거

--- streamlit/stramlit.py
+++ streamlit/stramlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 
-search_page = st.Page("search.py", title="검색", icon=":material/add_circle:")
-keyword_page = st.Page("keyword.py", title="키워드", icon=":material/add_circle:")
-home_page = st.Page("home.py", title="홈", icon=":material/house:")
-llm_page = st.Page("llm.py", title="LLM", icon=":material/add_circle:")
+search_page = st.Page("search.py", title="검색")
+keyword_page = st.Page("keyword.py", title="키워드")
+home_page = st.Page("home.py", title="홈")
+llm_page = st.Page("llm.py", title="LLM")
 
 pg = st.navigation([home_page, search_page, keyword_page, llm_page])
 pg.run()
